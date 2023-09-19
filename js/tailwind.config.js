d10e2eb69efb7e9f1716396eb4c6ff540fe5d12e
Josi-akash <136694531+Josi-akash@users.noreply.github.com>
2023-09-20 00:25:40 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,36 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      // color part start---
+      colors: {
+        headerColor: "rgba(22, 12, 109, 0.5)",
+        headerTextColor: "#FFFFFF",
+        bannerP:"#E6E5F3",
+        hoverColor: "#1BBF00",
+        overLy:"rgb(107, 98, 197,0.9)",
+      },
+
+    
+   
+      // color part end---
+      width: {
+        'container': '1170px',
+        bannercontainer: '962px',
+      },
+  //  bannar image----------
+  backgroundImage: {
+    bannerimage : "url('images/banner.png')",
+    
+  },
+  // google font------------
+  fontFamily: {
+    openF: ["Open Sans, sans-serif"],
+    paprika: ["Paprika, cursive"],
+  },
+
+
+
+
+    },
+  },
+};
